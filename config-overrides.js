75fafa4d034bfc241ccaed48c5b29905e337dd5b
fireairforce <1344492820@qq.com>
2019-08-31 21:03:27 +0800

    feat:添加antd和less一些配置项

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,18 @@
 /* config-overrides.js */
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
+const { resolve } = require('path');
 
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    console.log(env);
-    return config;
-  }
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+  }),
+  addWebpackAlias({
+    'utils':resolve(__dirname,'./src/utils'),
+    'pages':resolve(__dirname,'./src/pages')
+  }) 
+);
