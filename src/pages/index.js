ef1cac67c0365ff73ea5b333a2636ce933b5b4dc
fireairforce <1344492820@qq.com>
2019-09-01 11:40:24 +0800

    feat:fix demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React,{ Fragment } from 'react';
 import { Button } from 'antd';
-import styles from './index.module.less';
 
 function Main(){
     return(
         <Fragment>
-            <div className={styles.main}>
-
-            </div>
          <Button type="primary">
            艾浩nb
          </Button>
