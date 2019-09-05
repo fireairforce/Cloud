c7eb2258ca002b8ec6a69a15e9d732e2dabbd111
fireairforce <1344492820@qq.com>
2019-09-05 20:45:45 +0800

    feat:引入hooks

--- src/pages/value/value1.js
+++ src/pages/value/value1.js
@@ -1,12 +1,33 @@
-import React, { Fragment } from "react";
-import { Form, Input, Select } from "antd";
+import React, { Fragment, useState } from "react";
+import { Form, Input, Select, Upload, Icon } from "antd";
 import Verity from "utils/regex";
 import styles from "./style/value.module.less";
 
 const FormItem = Form.Item;
 const Option = Select.Option;
+const { TextArea } = Input;
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
 
 function ValueOne(props) {
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "image.png",
+      status: "done",
+      url:
+        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    }
+  ]);
   const { getFieldDecorator } = props.form;
   const formItemLayout = {
     labelCol: {
@@ -18,6 +39,7 @@ function ValueOne(props) {
       sm: { span: 16 }
     }
   };
+  // 选项权利
   const options = [
     {
       value: "玛瑙",
@@ -40,6 +62,24 @@ function ValueOne(props) {
       id: 4
     }
   ];
+  const handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewVisible(true);
+  };
+
+  const handleChange = ({ fileList }) => {
+    setFileList({ fileList });
+  };
+
+  const uploadButton = (
+    <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">Upload</div>
+    </div>
+  );
   return (
     <Fragment>
       <Form>
@@ -94,7 +134,17 @@ function ValueOne(props) {
                     message: "请上传宝石照片"
                   }
                 ]
-              })(<Input placeholder="例：透明到不透明/透明" />)}
+              })(
+                <Upload
+                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  listType="picture-card"
+                  fileList={fileList}
+                  onPreview={handlePreview}
+                  onChange={handleChange}
+                >
+                  {fileList.length >= 8 ? null : uploadButton}
+                </Upload>
+              )}
             </FormItem>
 
             <FormItem label="外观描述" {...formItemLayout}>
@@ -105,9 +155,13 @@ function ValueOne(props) {
                     message: "请填写外观描述"
                   }
                 ]
-              })(<Input placeholder="例：它呈浅-中等色调的蓝色，有时呈淡绿色，内含物少，单行输入" />)}
+              })(
+                <TextArea
+                  rows={4}
+                  placeholder="例：它呈浅-中等色调的蓝色，有时呈淡绿色，内含物少，单行输入"
+                />
+              )}
             </FormItem>
-
           </Form>
         </div>
       </Form>
